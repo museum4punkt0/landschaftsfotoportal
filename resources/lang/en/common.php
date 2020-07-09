262db72c6912fde373b93e6860f0fba9a0509356
sklemm <sebastian.klemm@senckenberg.de>
2020-07-09 16:04:25 +0200

    add some translated l10n strings

---
+++ resources/lang/en/common.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for common words and phrases.
+    |
+    */
+
+    'all' => 'all',
+    'none' => 'none',
+    'yes' => 'yes',
+    'no' => 'no',
+    'new' => 'new',
+    'show' => 'show',
+    'edit' => 'edit',
+    'delete' => 'delete',
+    'save' => 'save',
+    'upload' => 'upload',
+    'ignore' => 'ignore',
+    'internal' => 'internal',
+    'next' => 'next',
+
+    'root' => 'root element',
+    'id' => 'ID',
+    'name' => 'name',
+    'description' => 'description',
+    'actions' => 'actions',
+    'language' => 'language',
+    'ordering' => 'ordering',
+];
