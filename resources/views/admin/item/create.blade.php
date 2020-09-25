museum4punkt0/landschaftsfotoportal

commit f8fad0aeec265a2ff1c4beca1fe81e0612c6833c

add publish dropdown to create/edit forms and item controller in backend

resources/views/admin/item/create.blade.php
@@ -22,6 +22,20 @@
         <span class="text-danger">{{ $errors->first('title') }}</span>
     </div>
     <div class="form-group">
+        <span>@lang('common.published')</span>
+        <select name="public" class="form-control" size=1 >
+            <option value="1"
+                @if(old('public', $item->public) == 1) selected @endif>
+                @lang('common.yes')
+            </option>
+            <option value="0"
+                @if(old('public', $item->public) == 0) selected @endif>
+                @lang('common.no')
+            </option>
+        </select>
+        <span class="text-danger">{{ $errors->first('parent') }}</span>
+    </div>
+    <div class="form-group">
         <span>@lang('lists.parent')</span>
         <select name="parent" class="form-control" size=1 >
             <option value="">@lang('common.root')</option>

app/Http/Controllers/Admin/ItemController.php
@@ -128,6 +128,7 @@ class ItemController extends Controller
         $validation_rules['title'] = 'nullable|string';
         $validation_rules['parent'] = 'nullable|integer';
         $validation_rules['taxon'] = 'nullable|integer';
+        $validation_rules['public'] = 'required|integer';
         $validation_rules['fields'] = 'required|array';
         
         // Validation rules for all fields associated with columns
@@ -149,6 +150,7 @@ class ItemController extends Controller
             'title' => $request->input('title'),
             'parent_fk' => $request->input('parent'),
             'taxon_fk' => $request->input('taxon'),
+            'public' => $request->input('public'),
             'item_type_fk' => $item_type,
             'created_by' => $request->user()->id,
             'updated_by' => $request->user()->id,
@@ -398,6 +400,7 @@ class ItemController extends Controller
         $validation_rules['title'] = 'nullable|string';
         $validation_rules['parent'] = 'nullable|integer';
         $validation_rules['taxon'] = 'nullable|integer';
+        $validation_rules['public'] = 'required|integer';
         $validation_rules['fields'] = 'required|array';
         
         // Validation rules for all fields associated with columns
@@ -417,6 +420,7 @@ class ItemController extends Controller
         $item->title = $request->input('title');
         $item->parent_fk = $request->input('parent');
         $item->taxon_fk = $request->input('taxon');
+        $item->public = $request->input('public');
         $item->updated_by = $request->user()->id;
         $item->save();
         

resources/views/admin/item/edit.blade.php
@@ -13,6 +13,20 @@
         <span class="text-danger">{{ $errors->first('title') }}</span>
     </div>
     <div class="form-group">
+        <span>@lang('common.published')</span>
+        <select name="public" class="form-control" size=1 >
+            <option value="1"
+                @if(old('public', $item->public) == 1) selected @endif>
+                @lang('common.yes')
+            </option>
+            <option value="0"
+                @if(old('public', $item->public) == 0) selected @endif>
+                @lang('common.no')
+            </option>
+        </select>
+        <span class="text-danger">{{ $errors->first('parent') }}</span>
+    </div>
+    <div class="form-group">
         <span>@lang('lists.parent')</span>
         <select name="parent" class="form-control" size=1 >
             <option value="">@lang('common.root')</option>

commit fb325ef562ca6f39207a16665d135cb74e526ba6

fix bug in form of item create blade, introduced in f8fad0ae

resources/views/admin/item/create.blade.php
@@ -25,11 +25,11 @@
         <span>@lang('common.published')</span>
         <select name="public" class="form-control" size=1 >
             <option value="1"
-                @if(old('public', $item->public) == 1) selected @endif>
+                @if(old('public') == 1) selected @endif>
                 @lang('common.yes')
             </option>
             <option value="0"
-                @if(old('public', $item->public) == 0) selected @endif>
+                @if(old('public') == 0) selected @endif>
                 @lang('common.no')
             </option>
         </select>