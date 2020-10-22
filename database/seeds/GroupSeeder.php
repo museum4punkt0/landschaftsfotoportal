9acaeff297d27129b36e67dfb4352a9bbd0a658c
sklemm <sebastian.klemm@senckenberg.de>
2020-10-22 14:31:39 +0200

    add database seeder for user groups

---
+++ database/seeds/GroupSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create default user groups
+        DB::table('groups')->insert([
+            [
+                'name' => 'registered',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'permissions' => '{}',
+            ], [
+                'name' => 'author',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'permissions' => '{"show-dashboard": true}',
+            ], [
+                'name' => 'editor',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'permissions' => '{"show-dashboard": true}',
+            ], [
+                'name' => 'publisher',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'permissions' => '{"show-dashboard": true}',
+            ], [
+                'name' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'permissions' => '{"show-dashboard": true}',
+            ], [
+                'name' => 'super-admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'permissions' => '{"show-dashboard": true}',
+            ]
+        ]);
+    }
+}
