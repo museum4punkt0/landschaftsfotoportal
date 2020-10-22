0113ac2fcd46c5a03b8f0434b3e3d7bbcefba39f
sklemm <sebastian.klemm@senckenberg.de>
2020-10-22 15:05:30 +0200

    add database seeder for list attributes

---
+++ database/seeds/AttributeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AttributeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create default attributes for lists
+        DB::table('attributes')->insert([
+            ['name' => 'code', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'config', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'name_de', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'name_en', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
