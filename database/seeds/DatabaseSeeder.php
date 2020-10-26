27724b48c58891f90f77f65bc53d1db4663b567f
sklemm <sebastian.klemm@senckenberg.de>
2020-10-26 10:52:10 +0100

    add database seeder for internal lists and data types

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GroupSeeder::class,
             AttributeSeeder::class,
+            ListSeeder::class,
         ]);
     }
 }
---
+++ database/seeds/ListSeeder.php
@@ -0,0 +1,298 @@
+<?php
+
+use App\Attribute;
+use App\Element;
+use App\Selectlist;
+use Illuminate\Database\Seeder;
+
+class ListSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create default lists
+        $data_type_list = Selectlist::create([
+            'name' => '_data_type_',
+            'description' => 'Daten-Typen für Anzeigefelder',
+            'hierarchical' => false,
+            'internal' => true,
+        ]);
+        $item_type_list = Selectlist::create([
+            'name' => '_item_type_',
+            'description' => 'Inhalts-Typen für Datensätze',
+            'hierarchical' => false,
+            'internal' => true,
+        ]);
+        $translation_list = Selectlist::create([
+            'name' => '_translation_',
+            'description' => 'Übersetzte Namen und Beschreibungen für Anzeigespalten',
+            'hierarchical' => false,
+            'internal' => true,
+        ]);
+        $column_group_list = Selectlist::create([
+            'name' => '_column_group_',
+            'description' => 'Gliederungspunkte für Anzeigefelder',
+            'hierarchical' => false,
+            'internal' => true,
+        ]);
+        
+        // Create list elements for data types and their names
+        
+        // Data type: list
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_list_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Auswahlliste']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'list value']
+        );
+        // Data type: integer
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_integer_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Ganzzahlwert']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'integer value']
+        );
+        // Data type: float
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_float_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Fließkommawert']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'float value']
+        );
+        // Data type: string
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_string_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Zeichenkette']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'text value']
+        );
+        // Data type: date
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_date_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Datumswert']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'date value']
+        );
+        // Data type: url
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_url_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Weblink (URL)']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'web link (URL)']
+        );
+        // Data type: image
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_image_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Bilddatei']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'image file']
+        );
+        // Data type: map
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_map_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Karte']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'map']
+        );
+        // Data type: taxon
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_taxon_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Taxon']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'taxon']
+        );
+        // Data type: html
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_html_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'HTML Text']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'HTML text']
+        );
+        // Data type: image title
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_image_title_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Bildtitel']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'image title']
+        );
+        // Data type: image copyright
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_image_copyright_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Bildrechte']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'image copyright']
+        );
+        // Data type: image ppi
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_image_ppi_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Bild-ppi (Pixel pro Zoll)']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'image ppi (pixels per inch)']
+        );
+        // Data type: redirect
+        $element = Element::create([
+            'list_fk' => $data_type_list->list_id,
+            'parent_fk' => null,
+            'value_summary' => '',
+        ]);
+        $element->attributes()->attach(
+            Attribute::where('name', 'code')->value('attribute_id'),
+            ['value' => '_redirect_']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_de')->value('attribute_id'),
+            ['value' => 'Weiterleitungsziel']
+        );
+        $element->attributes()->attach(
+            Attribute::where('name', 'name_en')->value('attribute_id'),
+            ['value' => 'target for redirection']
+        );
+    }
+}
