ce03ef1a8bd0c413546cdab05a30de6810a69e30
sklemm <sebastian.klemm@senckenberg.de>
2021-01-25 20:33:40 +0100

    move modals for cart to blade subview

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,80 +2,8 @@
 
 @section('content')
 
-    <!-- Modal for showing an alert message -->
-    <div class="modal fade" id="alertModal" tabindex="-1" aria-labelledby="alertModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="alertModalLabel"></h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body" id="alertModalContent"></div>
-            </div>
-        </div>
-    </div>
-    
-    <!-- Modal for removing an item from cart -->
-    <div class="modal fade" id="cartRemoveModal" tabindex="-1" aria-labelledby="cartRemoveModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="cartRemoveModalLabel">@lang('cart.remove')</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <span>@lang('cart.confirm_remove')</span>
-                </div>
-                <div class="modal-footer">
-                    <div class="form-group">
-                        <input type="hidden" id="cartRemoveUrl" value="" />
-                        <button type="submit" class="btn btn-danger btn-submit" id="cartRemoveBtn">@lang('common.delete')</button>
-                    </div>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">@lang('common.cancel')</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <script type="text/javascript">
-        // Triggered when comment modal is shown
-        $('#cartRemoveModal').on('shown.bs.modal', function(event) {
-            // Store the URL for the AJAX request
-            var url = $(event.relatedTarget).data('href');
-            $('#cartRemoveUrl').val(url);
-        });
-        
-        // Removing items from cart
-        $("#cartRemoveBtn").click(function (xhr) {
-            xhr.preventDefault();
-            $.ajax({
-                type:'POST',
-                url:$('#cartRemoveUrl').val(),
-                //data:{foo:null},
-                success:function (data) {
-                    $('#cartRemoveModal').modal('hide');
-                    $('#alertModalLabel').text('@lang("cart.remove")');
-                    $('#alertModalContent').html('<div class="alert alert-success">' + data.success + '</div>');
-                    $('#alertModal').modal('show');
-                    // Close modal dialog
-                    window.setTimeout(function () {
-                        $('#alertModal').modal('hide');
-                        location.reload(true);
-                    }, 2500);
-                    
-                },
-                error:function (xhr) {
-                    $.each(xhr.responseJSON.errors, function (field, error) {
-                        // Render the error messages
-                        $('#alertModalContent').append('<div class="alert alert-danger">' + error + '</div>');
-                    });
-                },
-            });
-        });
-    </script>
+    @include('includes.modal_alert')
+    @include('includes.modal_cart_remove')
     
     <!-- Modal for adding a comment -->
     <div class="modal fade" id="commentModal" tabindex="-1" aria-labelledby="commentModalLabel" aria-hidden="true">
---
+++ resources/views/includes/modal_alert.blade.php
@@ -0,0 +1,14 @@
+<!-- Modal for showing an alert message -->
+<div class="modal fade" id="alertModal" tabindex="-1" aria-labelledby="alertModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="alertModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" id="alertModalContent"></div>
+        </div>
+    </div>
+</div>
