museum4punkt0/landschaftsfotoportal

commit e5c02afd17d88d76fd8355f285679cf6d925c8c0

add thumbnails to admin's item blade views

resources/views/admin/item/list.blade.php
@@ -24,6 +24,7 @@
                 <thead>
                     <tr>
                         <th colspan="1">@lang('common.id')</th>
+                        <th colspan="1"></th>
                         <th colspan="1">@lang('items.item_type')</th>
                         <th colspan="1">@lang('common.name')</th>
                         <th colspan="3">@lang('common.actions')</th>
@@ -34,6 +35,14 @@
                     <tr>
                         <td>
                             {{$item->item_id}}
+                        </td>
+                        <td>
+                            <div class="portfolio-item">
+                            <a class="portfolio-link d-flex justify-content-center" href="{{route('item.show.public', $item->item_id)}}#details">
+                                <img src="{{ asset('storage/'. Config::get('media.preview_dir') .
+                                    $item->details->firstWhere('column_fk', 13)->value_string .'.jpg') }}" height=100 alt="" title="{{ $item->details->firstWhere('column_fk', 23)->value_string }}"/>
+                            </a>
+                            </div>
                         </td>
                         <td>
                             @foreach($item->item_type->values as $v)

resources/views/admin/item/publish.blade.php
@@ -24,6 +24,7 @@
                 <thead>
                     <tr>
                         <th colspan="1">@lang('common.id')</th>
+                        <th colspan="1"></th>
                         <th colspan="1">@lang('items.item_type')</th>
                         <th colspan="1">@lang('common.name')</th>
                         <th colspan="1">@lang('common.updated')</th>
@@ -35,6 +36,14 @@
                     <tr>
                         <td>
                             {{$item->item_id}}
+                        </td>
+                        <td>
+                            <div class="portfolio-item">
+                            <a class="portfolio-link d-flex justify-content-center" href="{{route('item.show.public', $item->item_id)}}#details">
+                                <img src="{{ asset('storage/'. Config::get('media.preview_dir') .
+                                    $item->details->firstWhere('column_fk', 13)->value_string .'.jpg') }}" height=100 alt="" title="{{ $item->details->firstWhere('column_fk', 23)->value_string }}"/>
+                            </a>
+                            </div>
                         </td>
                         <td>
                             @foreach($item->item_type->values as $v)

commit b36584fb741f5452ed0849a6714e219e757d6c73

add workaround for errors introduced in e5c02afd

resources/views/admin/item/list.blade.php
@@ -39,8 +39,10 @@
                         <td>
                             <div class="portfolio-item">
                             <a class="portfolio-link d-flex justify-content-center" href="{{route('item.show.public', $item->item_id)}}#details">
+                            @if($item->details->firstWhere('column_fk', 13))
                                 <img src="{{ asset('storage/'. Config::get('media.preview_dir') .
                                     $item->details->firstWhere('column_fk', 13)->value_string .'.jpg') }}" height=100 alt="" title="{{ $item->details->firstWhere('column_fk', 23)->value_string }}"/>
+                            @endif
                             </a>
                             </div>
                         </td>

resources/views/admin/item/publish.blade.php
@@ -40,8 +40,10 @@
                         <td>
                             <div class="portfolio-item">
                             <a class="portfolio-link d-flex justify-content-center" href="{{route('item.show.public', $item->item_id)}}#details">
+                            @if($item->details->firstWhere('column_fk', 13))
                                 <img src="{{ asset('storage/'. Config::get('media.preview_dir') .
                                     $item->details->firstWhere('column_fk', 13)->value_string .'.jpg') }}" height=100 alt="" title="{{ $item->details->firstWhere('column_fk', 23)->value_string }}"/>
+                            @endif
                             </a>
                             </div>
                         </td>

resources/views/item/gallery.blade.php
@@ -11,6 +11,7 @@
             </div>
             <div class="row">
             @foreach($items as $item)
+                @if($item->details->firstWhere('column_fk', 13))
                 <div class="col-lg-4 col-sm-6 mb-4">
                     <div class="portfolio-item">
                         <a class="portfolio-link d-flex justify-content-center" href="{{route('item.show.public', $item->item_id)}}#details">
@@ -77,6 +78,7 @@
                         </div>
                     </div>
                 </div>
+                @endif
             @endforeach
             </div>
         </div>

resources/views/item/own.blade.php
@@ -11,6 +11,7 @@
             </div>
             <div class="row">
             @foreach($items as $item)
+                @if($item->details->firstWhere('column_fk', 13))
                 <div class="col-lg-4 col-sm-6 mb-4">
                     <div class="portfolio-item">
                         <a class="portfolio-link d-flex justify-content-center" href="{{route('item.show.public', $item->item_id)}}#details">
@@ -63,6 +64,7 @@
                         </div>
                     </div>
                 </div>
+                @endif
             @endforeach
             </div>
             <!-- Pagination -->