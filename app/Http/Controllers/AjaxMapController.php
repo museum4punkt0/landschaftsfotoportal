37048bb75d9ffbbdeb77c55947c8dcbddd9b5772
sklemm <sebastian.klemm@senckenberg.de>
2021-06-14 12:08:17 +0200

    catch exceptions in AJAX map on missing lat/lon details

--- app/Http/Controllers/AjaxMapController.php
+++ app/Http/Controllers/AjaxMapController.php
@@ -21,23 +21,34 @@ class AjaxMapController extends Controller
         
         // Create feature array for GeoJSON
         foreach ($items as $item) {
-            $features[] = [
-                'type' => 'Feature',
-                'id' => $item->item_id,
-                'properties' => [
-                    'name' => $item->details->firstWhere('column_fk', 23)->value_string,
-                    'preview' => asset('storage/'. config('media.preview_dir')) . '/'
-                        . $item->details->firstWhere('column_fk', 13)->value_string,
-                    'details' => route('item.show.public', $item),
-                ],
-                'geometry' => [
-                    'type' => 'Point',
-                    'coordinates' => [
-                        floatval($item->details->firstWhere('column_fk', 25)->value_float),
-                        floatval($item->details->firstWhere('column_fk', 24)->value_float),
+            // Check for missing lat/lon details
+            if ($item->details->firstWhere('column_fk', 25) && $item->details->firstWhere('column_fk', 24)) {
+                $features[] = [
+                    'type' => 'Feature',
+                    'id' => $item->item_id,
+                    'properties' => [
+                        'name' => $item->details->firstWhere('column_fk', 23)->value_string,
+                        'preview' => asset('storage/'. config('media.preview_dir')) . '/'
+                            . $item->details->firstWhere('column_fk', 13)->value_string,
+                        'details' => route('item.show.public', $item),
                     ],
-                ],
-            ];
+                    'geometry' => [
+                        'type' => 'Point',
+                        'coordinates' => [
+                            floatval($item->details->firstWhere('column_fk', 25)->value_float),
+                            floatval($item->details->firstWhere('column_fk', 24)->value_float),
+                        ],
+                    ],
+                ];
+            }
+            else {
+                /* for debugging
+                $features[] = [
+                    'type' => 'MISSING',
+                    'id' => $item->item_id,
+                ];
+                */
+            }
         }
         // ...and put all those in a GeoJSON feature collection
         $geojson = [
