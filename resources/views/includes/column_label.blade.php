dd7ce7a5306d6d9d20b188a88e47d38403a36097
sklemm <sebastian.klemm@senckenberg.de>
2021-10-04 10:55:34 +0200

    remove icon with tooltip for description above form input fields

--- resources/views/includes/column_label.blade.php
+++ resources/views/includes/column_label.blade.php
@@ -10,9 +10,11 @@
         @lang('columns.data_type'): 
         {{ $data_types->firstWhere('element_fk', $cm->column->data_type_fk)->value }})
     @endcan
-    
+
+    {{-- Not needed anymore: The description is already shown below the form input field
     @if($descriptions->firstWhere('element_fk', $cm->column->translation_fk))
         <i class="fas {{ Config::get('ui.icon_description', 'fa-info') }}"
             title="{{ $descriptions->firstWhere('element_fk', $cm->column->translation_fk)->value }}"></i>
     @endif
+    --}}
 </label>
