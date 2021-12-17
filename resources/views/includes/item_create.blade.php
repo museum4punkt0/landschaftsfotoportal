museum4punkt0/landschaftsfotoportal

commit ff3b6cca58bfe7076cbe0a61f2b07849c72e1fbc

remove useless (sub)headings from sub-blade includes in item create/edit forms

resources/views/includes/item_create.blade.php
@@ -2,8 +2,8 @@
 @unless (Route::currentRouteName() == 'item.create')
     @includeIf('includes.' . Config::get('ui.frontend_layout') . '.section_header', [
         'section_id' => 'edit_form',
-        'section_heading' => __('items.my_own'),
-        'section_subheading' => 'Lorem ipsum dolor sit amet consectetur.',
+        'section_heading' => _'',
+        'section_subheading' => '',
     ])
 @endunless
 

resources/views/includes/item_edit.blade.php
@@ -2,8 +2,8 @@
 @unless (Route::currentRouteName() == 'item.edit')
     @includeIf('includes.' . Config::get('ui.frontend_layout') . '.section_header', [
         'section_id' => 'edit_form',
-        'section_heading' => __('items.my_own'),
-        'section_subheading' => 'Lorem ipsum dolor sit amet consectetur.',
+        'section_heading' => '',
+        'section_subheading' => '',
     ])
 @endunless
 

commit f57363796a8fd6c60b53904c99d333ac11028cee

fix exception on creating items, introduced in ff3b6cca

resources/views/includes/item_create.blade.php
@@ -2,7 +2,7 @@
 @unless (Route::currentRouteName() == 'item.create')
     @includeIf('includes.' . Config::get('ui.frontend_layout') . '.section_header', [
         'section_id' => 'edit_form',
-        'section_heading' => _'',
+        'section_heading' => '',
         'section_subheading' => '',
     ])
 @endunless