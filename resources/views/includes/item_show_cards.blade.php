be82a5c8a237cc972fc425f38e4e53d64be467d8
sklemm <sebastian.klemm@senckenberg.de>
2022-09-15 18:39:59 +0200

    add integer scaling and precision to admin item show blade

--- resources/views/includes/item_show_cards.blade.php
+++ resources/views/includes/item_show_cards.blade.php
@@ -270,7 +270,15 @@
                 @include('includes.column_cardheader')
                 
                 <div class="card card-body">
+                @if($cm->getConfigValue('scale_factor'))
+                    {{ round(optional($details->firstWhere('column_fk', $cm->column->column_id))->value_int * $cm->getConfigValue('scale_factor'), $cm->getConfigValue('precision')) }}
+                    <span class="text-muted">
+                    @lang('colmaps.option_scale_factor_label'): {{ $cm->getConfigValue('scale_factor') }}, 
+                    @lang('colmaps.option_precision_label'): {{ $cm->getConfigValue('precision') }}
+                    </span>
+                @else
                     {{ optional($details->firstWhere('column_fk', $cm->column->column_id))->value_int }}
+                @endif
                 </div>
                 @break
             
